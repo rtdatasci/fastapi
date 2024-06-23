32236261195e269b44febbf2e472ce2f2e96227b
Rajeev Tajhya <rtdatasci@gmail.com>
2024-06-23 12:03:44 -0700

    another test

--- main.py
+++ main.py
@@ -54,4 +54,4 @@ def delete_task(task_id: UUID):
 if __name__ == "__main__":
     import uvicorn
 
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    uvicorn.run(app, host="127.0.0.1", port=8000)
